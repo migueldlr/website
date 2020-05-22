931488b28efd4f680d5d8c19e953a7d32fa01e91
mbclr3 <mbclr3@gatech.edu>
2020-05-22 16:58:20 -0400

    Add head

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { withTheme } from 'emotion-theming';
 import {
     Link as ChakraLink,
@@ -18,46 +19,51 @@ import { CTA } from '../components/CTA';
 import { Footer } from '../components/Footer';
 
 const Index = () => (
-    <Container>
-        <Avatar name="Miguel de los Reyes" src="/mig.jpg" />
-        <Hero />
-        <Main>
-            <Text>Miguel's website</Text>
-            <Text>
-                Example repository of <Code>Next.js</Code> +{' '}
-                <Code>chakra-ui</Code>.
-            </Text>
+    <>
+        <Head>
+            <title>Miguel's website</title>
+        </Head>
+        <Container>
+            <Avatar name="Miguel de los Reyes" src="/mig.jpg" />
+            <Hero />
+            <Main>
+                <Text>Miguel's website</Text>
+                <Text>
+                    Example repository of <Code>Next.js</Code> +{' '}
+                    <Code>chakra-ui</Code>.
+                </Text>
 
-            <List spacing={3} my={0}>
-                <ListItem>
-                    <ListIcon icon="check-circle" color="green.500" />
-                    <ChakraLink
-                        isExternal
-                        href="https://chakra-ui.com"
-                        flexGrow={1}
-                        mr={2}>
-                        Chakra UI <Icon name="external-link" mx="2px" />
-                    </ChakraLink>
-                </ListItem>
-                <ListItem>
-                    <ListIcon icon="check-circle" color="green.500" />
-                    <ChakraLink
-                        isExternal
-                        href="https://nextjs.org"
-                        flexGrow={1}
-                        mr={2}>
-                        Next.js <Icon name="external-link" mx="2px" />
-                    </ChakraLink>
-                </ListItem>
-            </List>
-        </Main>
+                <List spacing={3} my={0}>
+                    <ListItem>
+                        <ListIcon icon="check-circle" color="green.500" />
+                        <ChakraLink
+                            isExternal
+                            href="https://chakra-ui.com"
+                            flexGrow={1}
+                            mr={2}>
+                            Chakra UI <Icon name="external-link" mx="2px" />
+                        </ChakraLink>
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon icon="check-circle" color="green.500" />
+                        <ChakraLink
+                            isExternal
+                            href="https://nextjs.org"
+                            flexGrow={1}
+                            mr={2}>
+                            Next.js <Icon name="external-link" mx="2px" />
+                        </ChakraLink>
+                    </ListItem>
+                </List>
+            </Main>
 
-        <DarkModeSwitch />
-        <Footer>
-            <Text>Next ❤️ Chakra</Text>
-        </Footer>
-        <CTA />
-    </Container>
+            <DarkModeSwitch />
+            <Footer>
+                <Text>Next ❤️ Chakra</Text>
+            </Footer>
+            <CTA />
+        </Container>
+    </>
 );
 
 export default withTheme(Index);
